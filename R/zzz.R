7b999136a05a897da19c693e7af6e49941630fdd
Robin J. Evans <evans@stats.ox.ac.uk>
2022-11-18 14:37:27 +0000

    Removed .onLoad function

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,3 @@
 ##' @import rje
 ##' @importFrom utils head
 ##' @importFrom stats rgamma
-
-.onLoad <- function(libname, pkgname) {
-  vig_list = tools::vignetteEngine(name='knitr', package = 'knitr')
-  vweave <- vig_list[['knitr::knitr']][c('weave')][[1]]
-  vtangle <- vig_list[['knitr::knitr']][c('tangle')][[1]]
-  tools::vignetteEngine(pkgname, weave = vweave, tangle = vtangle,
-                        pattern = "[.]Rmd$", package = pkgname)
-  #register_vignette_engines(pkgname)
-}
